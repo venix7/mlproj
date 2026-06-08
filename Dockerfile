13af5136077972a95fe56966881d2b2014653728
Uday Banerjee <udayx4@gmail.com>
2026-06-08 23:46:54 +0530

    workflows and dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.11-slim
+WORKDIR /app
+COPY . .
+
+RUN apt update -y && apt install awscli -y
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "application.py"]
--- application.py
+++ application.py
@@ -38,4 +38,4 @@ def predict_datapoint():
         results = predict_pipeline.predict(features=pred_df)
         return render_template('home.html', results=results[0])
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
